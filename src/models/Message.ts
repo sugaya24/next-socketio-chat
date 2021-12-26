55797fe34d5decaa5dcbf8e78772775db2b2a1cd
YukiSugaya <sugaya324@gmail.com>
2021-12-26 14:41:59 +0900

    refactor: mod MessageModel

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -3,27 +3,28 @@ import mongoose, { Document, Model, models, Schema } from 'mongoose';
 export interface IMessage extends Document {
   username: string;
   messageText: string;
-  createdAt?: Date;
-  _id?: string;
+  roomId: string;
 }
 
-const messageScheme: Schema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, `Please add a username`],
+const messageSchema: Schema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, `Please add a username`],
+    },
+    messageText: {
+      type: String,
+      required: [true, `Please add a message`],
+    },
+    roomId: {
+      type: String,
+      required: [true, `Please add roomId`],
+    },
   },
-  messageText: {
-    type: String,
-    required: [true, `Please add a message`],
-  },
-  createdAt: {
-    type: Date,
-    default: new Date(),
-    required: [true, `Please add a created date`],
-  },
-});
+  { timestamps: true },
+);
 
 type MessageModel = Model<IMessage>;
 export default models.Message
   ? (models.Message as MessageModel)
-  : mongoose.model<IMessage>(`Message`, messageScheme);
+  : mongoose.model<IMessage>(`Message`, messageSchema);
